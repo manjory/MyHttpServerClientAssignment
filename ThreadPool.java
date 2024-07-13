54b9bf15dc16797784dcb5fe97d231d01a75bc64
Immaculate <manjory.sjsu@gmail.com>
2024-07-13 06:20:01 -0700

    Adding threadPool

---
+++ ThreadPool.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.*;
+
+public class ThreadPool {
+    private final ExecutorService executor;
+
+    public ThreadPool(int nThreads) {
+        executor = Executors.newFixedThreadPool(nThreads);
+    }
+
+    public void execute(Runnable task) {
+        executor.execute(task);
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+    }
+}
